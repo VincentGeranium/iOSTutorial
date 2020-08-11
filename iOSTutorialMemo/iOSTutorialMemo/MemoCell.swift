ab26e52ccfc5909a161c06126d57715074cd1b77
김광준 <kwangjun3952@gmail.com>
2020-08-11 18:44:25 +0900

    셀의 글의 제목 레이블과 글의 내용 레이블 UI 작업 완료

--- iOSTutorialMemo/iOSTutorialMemo/MemoCell.swift
+++ iOSTutorialMemo/iOSTutorialMemo/MemoCell.swift
@@ -12,17 +12,27 @@ class MemoCell: UITableViewCell {
     
     static let reuseIdentifier: String = "ListTableViewCell"
     
-    let titleLabel: UILabel = {
+    lazy var titleLabel: UILabel = {
         var titleLabel: UILabel = UILabel()
         titleLabel.font = UIFont.boldSystemFont(ofSize: 14.0)
         titleLabel.backgroundColor = UIColor.systemBlue
         return titleLabel
     }()
     
+    lazy var contentsLabel: UILabel = {
+        var contentsLabel: UILabel = UILabel()
+        contentsLabel.font = UIFont.systemFont(ofSize: 12.0)
+        contentsLabel.numberOfLines = 2
+        contentsLabel.lineBreakMode = NSLineBreakMode.byTruncatingTail
+        contentsLabel.backgroundColor = UIColor.systemRed
+        return contentsLabel
+    }()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         self.accessoryType = UITableViewCell.AccessoryType.disclosureIndicator
-        setUpTitleLabel()
+        setUpAndAddSubviews()
+        
     }
     
     required init?(coder: NSCoder) {
@@ -33,26 +43,43 @@ class MemoCell: UITableViewCell {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
+    }
+    
+    private func setUpAndAddSubviews() {
+        setUpTitleLabel()
+        setUpContentsLabel()
     }
     
     private func setUpTitleLabel() {
         let guide = self.contentView.safeAreaLayoutGuide
-        
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
-    
         self.contentView.addSubview(titleLabel)
         
         NSLayoutConstraint.activate([
             titleLabel.topAnchor.constraint(equalTo: guide.topAnchor, constant: 5),
             titleLabel.leadingAnchor.constraint(equalTo: guide.leadingAnchor, constant: 5),
-            titleLabel.heightAnchor.constraint(equalToConstant: 40),
+            titleLabel.heightAnchor.constraint(equalToConstant: 35),
             titleLabel.widthAnchor.constraint(equalToConstant: (contentView.frame.width) * 2/3),
         ])
     }
-
+    
+    private func setUpContentsLabel() {
+        let guide = self.contentView.safeAreaLayoutGuide
+        contentsLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.contentView.addSubview(contentsLabel)
+        
+        NSLayoutConstraint.activate([
+            contentsLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 5),
+            contentsLabel.leadingAnchor.constraint(equalTo: guide.leadingAnchor, constant: 5),
+            contentsLabel.heightAnchor.constraint(equalTo: titleLabel.heightAnchor),
+            contentsLabel.widthAnchor.constraint(equalTo: titleLabel.widthAnchor),
+            
+        ])
+    }
+    
 }
--- iOSTutorialMemo/iOSTutorialMemo/MemoWithImageCell.swift
+++ iOSTutorialMemo/iOSTutorialMemo/MemoWithImageCell.swift
@@ -12,15 +12,32 @@ class MemoWithImageCell: UITableViewCell {
     
     static let reuseIdentifier: String = "MemoWithImageCell"
     
-    let titleImageView: UIImageView = {
+    lazy var titleImageView: UIImageView = {
         var titleImageView: UIImageView = UIImageView()
         titleImageView.backgroundColor = .black
         return titleImageView
     }()
     
+    lazy var titleLabel: UILabel = {
+        var titleLabel: UILabel = UILabel()
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 14.0)
+        titleLabel.backgroundColor = UIColor.systemBlue
+        return titleLabel
+    }()
+    
+    lazy var contentsLabel: UILabel = {
+        var contentsLabel: UILabel = UILabel()
+        contentsLabel.font = UIFont.systemFont(ofSize: 12.0)
+        contentsLabel.lineBreakMode = NSLineBreakMode.byTruncatingTail
+        contentsLabel.numberOfLines = 2
+        contentsLabel.backgroundColor = UIColor.systemRed
+        return contentsLabel
+    }()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        setUpTitleImageView()
+        self.accessoryType = UITableViewCell.AccessoryType.disclosureIndicator
+        setUpAndAddSubviews()
     }
     
     required init?(coder: NSCoder) {
@@ -31,26 +48,57 @@ class MemoWithImageCell: UITableViewCell {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
+    }
+    
+    // All SetUp Functions
+    private func setUpAndAddSubviews() {
+        setUpTitleImageView()
+        setUpTitleLabel()
+        setUpContentsLabel()
     }
     
     private func setUpTitleImageView() {
         let guide = self.contentView.safeAreaLayoutGuide
-        
         titleImageView.translatesAutoresizingMaskIntoConstraints = false
-        
         self.contentView.addSubview(titleImageView)
         
         NSLayoutConstraint.activate([
             titleImageView.topAnchor.constraint(equalTo: guide.topAnchor, constant: 5),
-            titleImageView.leadingAnchor.constraint(equalTo: guide.leadingAnchor, constant: 10),
+            titleImageView.leadingAnchor.constraint(equalTo: guide.leadingAnchor, constant: 5),
             titleImageView.bottomAnchor.constraint(equalTo: guide.bottomAnchor, constant: -5),
             titleImageView.widthAnchor.constraint(equalToConstant: contentView.frame.width / 5),
         ])
     }
-
+    
+    private func setUpTitleLabel() {
+        let guide = self.contentView.safeAreaLayoutGuide
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.contentView.addSubview(titleLabel)
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: guide.topAnchor, constant: 5),
+            titleLabel.leadingAnchor.constraint(equalTo: titleImageView.trailingAnchor, constant: 5),
+            titleLabel.widthAnchor.constraint(equalToConstant: (contentView.frame.width) * 2/3),
+            titleLabel.heightAnchor.constraint(equalToConstant: 35)
+        ])
+    }
+    
+    private func setUpContentsLabel() {
+        let guide = self.contentView.safeAreaLayoutGuide
+        contentsLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.contentView.addSubview(contentsLabel)
+        
+        NSLayoutConstraint.activate([
+            contentsLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 5),
+            contentsLabel.leadingAnchor.constraint(equalTo: titleImageView.trailingAnchor, constant: 5),
+            contentsLabel.widthAnchor.constraint(equalTo: titleLabel.widthAnchor),
+            contentsLabel.heightAnchor.constraint(equalTo: titleLabel.heightAnchor),
+        ])
+    }
+    
 }
