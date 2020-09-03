0ea7f3d35ff67f44ed8bb88fe9cc1179f39b1b3f
김광준 <kwangjun3952@gmail.com>
2020-09-03 23:12:45 +0900

    Protocol Delegate Pattern Tutorial init

---
+++ ProtocolDelegatePattern/ProtocolDelegatePattern/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProtocolDelegatePattern
+//
+//  Created by 김광준 on 2020/09/03.
+//  Copyright © 2020 VincentGeranium. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
